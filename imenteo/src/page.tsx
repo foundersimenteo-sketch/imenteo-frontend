131588c2a09e7a3d6c098de4d9f63f90e14b3782
Ayush <mailayush1234@gmail.com>
2025-10-30 14:06:32 +0530

    feat: page.tsx

---
+++ imenteo/src/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Welcome to the Imenteo Frontend Page</h1>
+      <p>This is the main page of the Imenteo frontend application.</p>
+    </div>
+  )
+}
+
+export default page
